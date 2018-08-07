463ba0503fb0ee85c0decca5e4656ee5d9d6f18b
Simon Vrachliotis <simon@simonswiss.com>
2018-08-08 09:14:49 +1000

    adding link to git repo from netlify demo page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app" class="max-w-md mx-auto px-4 py-8 font-sans leading-normal">
     <h1 class="text-3xl text-purple-darker leading-tight font-black mb-6">Component library with Vue, Tailwind and Storybook</h1>
-    <Button class="mr-2 mb-2" color="purple">How</Button>
-    <Button class="mr-2 mb-2" color="indigo">cool</Button>
-    <Button class="mr-2 mb-2" color="blue">is this</Button>
-    <Button class="mr-2 mb-2" color="teal">huh?</Button>
+    <Button @click="handleClick" class="mr-2 mb-2" color="purple">How</Button>
+    <Button @click="handleClick" class="mr-2 mb-2" color="indigo">cool</Button>
+    <Button @click="handleClick" class="mr-2 mb-2" color="blue">is this</Button>
+    <Button @click="handleClick" class="mr-2 mb-2" color="teal">huh?</Button>
+
+    <p class="text-grey darker mt-2"><span class="text-3xl">☝</span>️ Pick your flavour - they all take you to the same github repo!</p>
   </div>
 </template>
 
@@ -17,6 +19,11 @@ export default {
   name: "app",
   components: {
     Button
+  },
+  methods: {
+    handleClick() {
+      window.location.href = "https://vue-storybook-tailwind.netlify.com/";
+    }
   }
 };
 </script>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,8 @@
 <template>
   <button 
-    class="py-3 px-4 text-white font-semibold shadow rounded" 
-    :class="`bg-${color} hover:bg-${color}-light`">
+    class="py-3 px-4 text-white font-semibold shadow rounded focus:outline-none" 
+    :class="`bg-${color} hover:bg-${color}-light`"
+    @click="$emit('click')">
     <slot></slot>
   </button>
 </template>
@@ -16,3 +17,10 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+button:focus,
+button:active {
+  box-shadow: 0 0 0 3px #d7d3dc;
+}
+</style>
